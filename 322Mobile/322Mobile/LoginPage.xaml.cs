22e2a98f07ee46244e66f56c6b60e86588410a29
Denizkusu <derin.denizkusu@gmail.com>
2019-02-25 12:19:25 -0500

    Add project files.

---
+++ 322Mobile/322Mobile/LoginPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _322Mobile
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            BackgroundColor.Equals("#FFF");
+            InitializeComponent();
+
+        }
+
+        void OnLoginButtonClicked(object sender, System.EventArgs e)
+        {
+            (temp).Text = username.Text + password.Text;
+        }
+    }
+}
---
+++ 322Mobile/322Mobile/RegisterPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace _322Mobile
+{
+	public partial class RegisterPage : ContentPage
+	{
+		public RegisterPage ()
+		{
+            InitializeComponent();
+
+		}
+
+        void OnRegisterButtonClicked(object sender, System.EventArgs e)
+        {
+            (temp).Text = username.Text + password.Text+email.Text;
+        }
+    }   
+}
