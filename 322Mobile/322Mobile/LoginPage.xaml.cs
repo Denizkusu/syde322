06a1a1e34d4564c404791081420344b629c38437
kevinluong96 <k5luong@uwaterloo.ca>
2019-02-27 14:34:13 -0500

    rename main page and try to use tabbed page

--- 322Mobile/322Mobile/LoginPage.xaml.cs
+++ 322Mobile/322Mobile/LoginPage.xaml.cs
@@ -10,69 +10,69 @@ using System.Text.RegularExpressions;
 
 namespace _322Mobile
 {
-    public partial class LoginPage : ContentPage
+  public partial class LoginPage : ContentPage
+  {
+    private static HttpClient client;
+    public LoginPage()
     {
-        private static HttpClient client;
-        public LoginPage()
-        {
-            client = new HttpClient();
-            BackgroundColor.Equals("#FFF");
-            InitializeComponent();
+      client = new HttpClient();
+      BackgroundColor.Equals("#FFF");
+      InitializeComponent();
 
-        }
+    }
 
-        void Handle_Unfocused_Email(object sender, Xamarin.Forms.FocusEventArgs e)
-        {
-            // Ensure email is of x@y.z
-            Regex rx = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
-          RegexOptions.Compiled | RegexOptions.IgnoreCase);
+    void Handle_Unfocused_Email(object sender, Xamarin.Forms.FocusEventArgs e)
+    {
+      // Ensure email is of x@y.z
+      Regex rx = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+    RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-            if (!rx.IsMatch(email.Text) && email.Text != "")
-            {
-                emailValidation.Text = "You must enter a valid email address";
-            }
-            else
-            {
-                emailValidation.Text = "";
-            }
-        }
+      if (!rx.IsMatch(email.Text) && email.Text != "")
+      {
+        emailValidation.Text = "You must enter a valid email address";
+      }
+      else
+      {
+        emailValidation.Text = "";
+      }
+    }
 
-        async void OnLoginButtonClicked(object sender, System.EventArgs e)
-        {
-            error.Text = "";
-            var values = new Dictionary<string, string>
+    async void OnLoginButtonClicked(object sender, System.EventArgs e)
+    {
+      error.Text = "";
+      var values = new Dictionary<string, string>
             {
                { "Username", email.Text },
                { "Password", password.Text }
             };
 
 
-            var content = new FormUrlEncodedContent(values);
+      var content = new FormUrlEncodedContent(values);
 
-            try
-            {
-                var response = await client.PostAsync("https://localhost:5001/api/login", content);
-                var responseString = await response.Content.ReadAsStringAsync();
-                Navigation.InsertPageBefore(new HomePage(), Navigation.NavigationStack[0]);
-                await Navigation.PopToRootAsync();
+      try
+      {
+        var response = await client.PostAsync("https://localhost:5001/api/login", content);
+        var responseString = await response.Content.ReadAsStringAsync();
+        Navigation.InsertPageBefore(new MainPage(), Navigation.NavigationStack[0]);
+        await Navigation.PopToRootAsync();
 
-            }
-            catch (WebException ex)
-            {
+      }
+      catch (WebException ex)
+      {
 
-                if (ex.Response is HttpWebResponse)
-                {
-                    var httpResponse = ex.Response as HttpWebResponse;
-                    error.Text =httpResponse.StatusDescription;
-                }
-                else
-                {
-                    error.Text = ex.Message;
-                }
-
-                await Navigation.PushAsync(new HomePage());
-
-            }
+        if (ex.Response is HttpWebResponse)
+        {
+          var httpResponse = ex.Response as HttpWebResponse;
+          error.Text = httpResponse.StatusDescription;
         }
+        else
+        {
+          error.Text = ex.Message;
+        }
+
+        await Navigation.PushAsync(new MainPage());
+
+      }
     }
+  }
 }
